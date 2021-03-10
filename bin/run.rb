6e02bc3a1e94e87be9a37aa5e25f8f087ce8d37b
Cori Blount <heycorihey@icloud.com>
2021-03-10 14:59:26 -0600

    newnew

--- bin/run.rb
+++ bin/run.rb
@@ -1,26 +1,5 @@
 require_relative '../config/environment'
 
 
-
-
-
-def welcome
-title = Artii::Base.new(:font => "slant")
-puts "Welcome to:"
-puts title.asciify("Vacation Booking")
-end
-
-def login
-    prompt = TTY::Prompt.new
-    question = prompt.select("What would you like to do?", %w(Login CreateAccount Exit))
-    if (question == "Login") 
-        puts "Hello"
-        login_name = prompt.ask("Enter Username") 
-            if User.include?(login_name)  
-        end
-    end
-end
-
-
-welcome
-login
+app = VacationBooking.new
+app.run
--- lib/vacationbooking.rb
+++ lib/vacationbooking.rb
@@ -1,12 +1,27 @@
 class VacationBooking #runs logic
     attr_reader :user, :destination, :trip, :sight, :flight
-
     def run
-        welcome
+        welcome 
+        login
     end
 
     def welcome
-        puts "Hello"
-    end
+        title = Artii::Base.new(:font => "slant")
+        puts "Welcome to:"
+        puts title.asciify("Vacation Booking")
+        end
+        
+        def login
+            prompt = TTY::Prompt.new
+            question = prompt.select("What would you like to do?", %w(Login CreateAccount Exit))
+            if (question == "Login") 
+                puts "Hello"
+                login_name = prompt.ask("Enter Username") 
+                login_password = prompt.ask("What is your password?")
+                    if User.all.include?(login_name) 
+                        puts "Your dream vacation is only a few clicks away!"
+                end
+            end
+        end
 
 end
